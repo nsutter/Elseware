50601dffc02499d317a872dde256e5889fb06d45
Théo Poizat <theo.poizat@zatteo.com>
2015-12-04 04:34:48 +0100

    dsqfsd

--- models/EventModel.js
+++ models/EventModel.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 module.exports = mongoose.model('Event',{
 	nom: String,
 	description: String,
+	description_admin: String,
 	statut: Number,
   longitude: Number,
   latitude: Number,
